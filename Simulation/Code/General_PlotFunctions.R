381f3130bb53d456ca5cd8e3091e8c41d8a4fde4
AlexSch2 <alex.schw40i@gmail.com>
2023-04-19 09:47:57 +0200

    Added more plotting functions

--- Simulation/Code/General_PlotFunctions.R
+++ Simulation/Code/General_PlotFunctions.R
@@ -1,3 +1,5 @@
+
+#This function plots the raw timeseries
 Plot.Timeseries <- function(Data_Raw,Id,Save=T,SubCategory=F,MainCategory=1,TextSize=50) {
   
   for(Id_RunVariable in Id) {
@@ -42,7 +44,7 @@ Plot.Timeseries <- function(Data_Raw,Id,Save=T,SubCategory=F,MainCategory=1,Text
 }
 
 
-
+#This plots the timeseries with the predictions made by the Ingarch model
 Plot.TimeseriesIngarch <- function(Data_Raw,IngarchResult,Id,Save=T,SubCategory=F,MainCategory=1,TextSize=50){
   
   for(Id_RunVariable in Id) {
@@ -78,7 +80,7 @@ Plot.TimeseriesIngarch <- function(Data_Raw,IngarchResult,Id,Save=T,SubCategory=
       geom_line(data=PlotData,aes(x=date,y=valueTrue),col="black",inherit.aes = F)+
       geom_point(data=PlotData,aes(x=date,y=valueTrue),col="black",inherit.aes = F)+
       theme(text = element_text(size =TextSize))+
-      ggtitle(paste("Timeseries with Ingarch Predictions",sep=" "),subtitle = paste("Fride ID",Id_RunVariable,Subtitle_Text,sep=" "))+
+      ggtitle(paste("Timeseries with INGARCH Predictions",sep=" "),subtitle = paste("Fridge ID",Id_RunVariable,Subtitle_Text,sep=" "))+
       ylab("Value")+
       xlab("Time")
     
@@ -91,4 +93,159 @@ Plot.TimeseriesIngarch <- function(Data_Raw,IngarchResult,Id,Save=T,SubCategory=
 }
 
 
+#This plots the timeseries with the predictions made by the Coda model
+Plot.TimeseriesCoda <- function(Data_Raw,CodaResult,Id,Save=T,TextSize=50){
+  
+  for(Id_RunVariable in Id) {
+    
+    Data_Raw <- Data_Raw %>% filter(fridge_id == Id_RunVariable)
+    
+    Category <- as.character(unique(Data_Raw$main_category_id))
+    Subtitle_Text <- ""
+    
+    #Preparing data for plotting
+    PlotData <- Data_Raw %>%
+      dplyr::select(fridge_id,week_date, main_category_id, sub_category_id, sold) %>%
+      Data.Preparation(Category = Category,TakeSubCategory = F) %>% 
+      pivot_longer(cols= all_of(Category),names_to="category",values_to="valueTrue")
+    
+    names(PlotData)[1] <- c("date")
+    
+    PlotDataCoda <- CodaResult$result %>% filter(id==Id_RunVariable & category %in% c(1,2,3,4))
+    
+    #Creating the plot
+    Plot <- ggplot(PlotDataCoda,aes(x=date,y=valuePredict))+
+      facet_wrap(vars(category),nrow=length(unique(PlotDataCoda$category)),scales = "free")+
+      geom_point(col="red")+
+      geom_line(col="red")+
+      geom_line(data=PlotData,aes(x=date,y=valueTrue),col="black",inherit.aes = F)+
+      geom_point(data=PlotData,aes(x=date,y=valueTrue),col="black",inherit.aes = F)+
+      theme(text = element_text(size =TextSize))+
+      ggtitle(paste("Timeseries with CoDA Predictions",sep=" "),subtitle = paste("Fridge ID",Id_RunVariable,Subtitle_Text,sep=" "))+
+      ylab("Value")+
+      xlab("Time")
+    
+    
+    #Saving of the plot
+    if(Save){
+      ggsave(filename = here("Plots",paste("Coda_Timeseries_ID",Id_RunVariable,".png",sep="")),plot=Plot,height = 15,width = 20)
+    }
+  }
+}
 
+
+#This function plots the timeseries with both models 
+Plot.TimeseriesCodaIngarch <- function(Data_Raw,CodaResult,IngarchResult,Id,Save=T,TextSize=50){
+  
+  for(Id_RunVariable in Id) {
+    
+    Data_Raw <- Data_Raw %>% filter(fridge_id == Id_RunVariable)
+    
+    Category <- as.character(unique(Data_Raw$main_category_id))
+    Subtitle_Text <- ""
+    
+    #Preparing data for plotting
+    PlotData <- Data_Raw %>%
+      dplyr::select(fridge_id,week_date, main_category_id, sub_category_id, sold) %>%
+      Data.Preparation(Category = Category,TakeSubCategory = F) %>% 
+      pivot_longer(cols= all_of(Category),names_to="category",values_to="valueTrue")
+    
+    names(PlotData)[1] <- c("date")
+    
+    PlotDataCoda_Columns <- names(CodaResult$result)
+    PlotDataIngarch_Columns <- names(IngarchResult$result)
+    Names_Joined <- intersect(PlotDataIngarch_Columns,PlotDataCoda_Columns)
+    
+    PlotDataCoda <- CodaResult$result %>% filter(id == Id_RunVariable &  category %in% c(1, 2, 3, 4)) %>%
+                                          dplyr::select(all_of(Names_Joined))
+    
+    PlotDataIngarch <- IngarchResult$result %>% filter(id==Id_RunVariable & category %in% c(1,2,3,4))%>% 
+                                                dplyr::select(all_of(Names_Joined))
+    
+    PlotDataBoth <- rbind(PlotDataCoda,PlotDataIngarch)
+    
+    #Creating the plot
+    MyColour <- setNames(c("red", "blue"),
+                        c("coda","ingarch"))
+    MyNames <- setNames(c("CoDA","INGARCH"),
+                        c("coda","ingarch"))
+    
+    Plot <- ggplot(PlotDataBoth,aes(x=date,y=valuePredict,col=model))+
+      facet_wrap(vars(category),nrow=length(unique(PlotDataCoda$category)),scales = "free")+
+      geom_point()+
+      geom_line(data=PlotDataBoth,aes(x=date,y=valuePredict,col=model))+
+      geom_line(data=PlotData,aes(x=date,y=valueTrue),col="black",inherit.aes = F)+
+      geom_point(data=PlotData,aes(x=date,y=valueTrue),col="black",inherit.aes = F)+
+      theme(text = element_text(size =TextSize))+
+      ggtitle(paste("Timeseries with both models",sep=" "),subtitle = paste("Fridge ID",Id_RunVariable,Subtitle_Text,sep=" "))+
+      ylab("Value")+
+      xlab("Time")+
+      scale_color_manual("Model", values = c(MyColour),labels=c(MyNames))
+    
+    
+    #Saving of the plot
+    if(Save){
+      ggsave(filename = here("Plots",paste("Both_Timeseries_ID",Id_RunVariable,".png",sep="")),plot=Plot,height = 15,width = 20)
+    }
+  }
+}
+
+
+#This function plots the timeseries with both models and their respective confidence bands
+Plot.TimeseriesCodaIngarchPI <- function(Data_Raw,CodaResult,IngarchResult,Id,Save=T,TextSize=50){
+  
+  for(Id_RunVariable in Id) {
+    
+    Data_Raw <- Data_Raw %>% filter(fridge_id == Id_RunVariable)
+    
+    Category <- as.character(unique(Data_Raw$main_category_id))
+    Subtitle_Text <- ""
+    
+    #Preparing data for plotting
+    PlotData <- Data_Raw %>%
+      dplyr::select(fridge_id,week_date, main_category_id, sub_category_id, sold) %>%
+      Data.Preparation(Category = Category,TakeSubCategory = F) %>% 
+      pivot_longer(cols= all_of(Category),names_to="category",values_to="valueTrue")
+    
+    names(PlotData)[1] <- c("date")
+    
+    PlotDataCoda_Columns <- names(CodaResult$result)
+    PlotDataIngarch_Columns <- names(IngarchResult$result)
+    Names_Joined <- intersect(PlotDataIngarch_Columns,PlotDataCoda_Columns)
+    
+    PlotDataCoda <- CodaResult$result %>% filter(id == Id_RunVariable &  category %in% c(1, 2, 3, 4)) %>%
+      dplyr::select(all_of(Names_Joined))
+    
+    PlotDataIngarch <- IngarchResult$result %>% filter(id==Id_RunVariable & category %in% c(1,2,3,4))%>% 
+      dplyr::select(all_of(Names_Joined))
+    
+    PlotDataBoth <- rbind(PlotDataCoda,PlotDataIngarch)
+    
+    #Creating the plot
+    MyColour <- setNames(c("red", "blue"),
+                         c("coda","ingarch"))
+    MyNames <- setNames(c("CoDA","INGARCH"),
+                        c("coda","ingarch"))
+    
+    Plot <- ggplot(PlotDataBoth,aes(x=date,y=valuePredict,col=model))+
+      facet_wrap(vars(category),nrow=length(unique(PlotDataCoda$category)),scales = "free")+
+      geom_point()+
+      geom_line(data=PlotDataBoth,aes(x=date,y=valuePredict,col=model))+
+      geom_ribbon(aes(x=date,y=valuePredict,col=model,ymin=lowerBound,ymax=upperBound,fill=model),
+                  data=PlotDataBoth,alpha=0.15,inherit.aes = F,linetype="dashed")+
+      geom_line(data=PlotData,aes(x=date,y=valueTrue),col="black",inherit.aes = F)+
+      geom_point(data=PlotData,aes(x=date,y=valueTrue),col="black",inherit.aes = F)+
+      theme(text = element_text(size =TextSize))+
+      ggtitle(paste("Timeseries with both models",sep=" "),subtitle = paste("Fridge ID",Id_RunVariable,Subtitle_Text,sep=" "))+
+      ylab("Value")+
+      xlab("Time")+
+      scale_color_manual("Model", values = c(MyColour),labels=c(MyNames))+
+      guides(fill="none")
+    
+    
+    #Saving of the plot
+    if(Save){
+      ggsave(filename = here("Plots",paste("BothPI_Timeseries_ID",Id_RunVariable,".png",sep="")),plot=Plot,height = 15,width = 20)
+    }
+  }
+}
